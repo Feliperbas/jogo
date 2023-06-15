e1e6f8810bdb532450738eefb3f8fb2bc396829d
Feliperbas <136742209+Feliperbas@users.noreply.github.com>
2023-06-15 14:19:31 -0300

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,55 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`200020000302020202020202020202020202020202020442424242424242424242424242090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0542424242424241414141414142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a05424242424242411c1e1d184142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a05424242424242411f2425194142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a05424242424242412026271a4142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a05424242424242412122231b4142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0542424242424241414040414142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0542424242424241414040414142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0542424242424241414040414142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0542424242424241414040414142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0542424242424241414040414142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0542424242424241414040414142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0542424242424241414040414142090a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a05141614424242414140404141420807070707070707070707070707070707070614141414141414414040414142010101010101010101010c0101011414161416141616141414144140404141420c0101010101010101010101010101011416141414171614141741404041414201010101010c010101010101010101011414161414141414141641404041414201010101010101010101010c0101010114161414151414141614414040414142010101010101010c0101010101010101141414141414141414144140404141420b0b0101010c01010101010101010101161617171614141614174140404141420b0b0b01010101010101010c01010114141714141414141414144140404141420b0b0b0b0101010101010101010b0b0b171416141414161616144040404141420b0b0b0b0b0b0b0b010101010b0b0b0b161414141414141414174040404141420b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b131212121212121211414140404141420b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b100d0d0d0d0d0d0d0d114140404141420b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b100d0d0d0d0d0d0d0d0d1c1e1d1e1d180b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b100d0d0d0d0d0d0d0d0d1f393a3a3b19282c2d2e0b0b0b0b0b0b0b0b0b0b0b0b100d0d0d0d0d0d0d0d0d203e38383c1a2b35342f0b0b0b0b0b0b0b0b0b0b0b0b100d0d0d0d0d0d0d0d0d1f3e38383c192a3736300b0b0b0b0b0b0b0b0b0b13120d0d0d0d0d0d0d0d0d0d203f38383d1a293332310b0b0b0b0b0b0b0b0b0b0e0f0f0f0f0f0f0f0f0f0f0f21222322231b`, img`
+................................
+.........................222222.
+.........................2....2.
+.........................2....2.
+.........................2....2.
+.........................2....2.
+.........................22..22.
+.........................22..22.
+.........................22..22.
+.........................22..22.
+.........................22..22.
+.........................22..22.
+.........................22..22.
+.........................22..22.
+..........................2..22.
+..........................2..22.
+..........................2..22.
+..........................2..22.
+..........................2..22.
+..........................2..22.
+..........................2..22.
+..........................2..22.
+.............................22.
+.............................22.
+.........................22..22.
+..........................2..22.
+................................
+...........................2222.
+...........................2222.
+...........................2222.
+...........................2222.
+................................
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath2,sprites.castle.tilePath1,sprites.castle.tilePath3,sprites.castle.tilePath6,sprites.castle.tilePath9,sprites.castle.tilePath8,sprites.castle.tilePath7,sprites.castle.tilePath4,sprites.castle.tilePath5,sprites.castle.tileDarkGrass2,sprites.builtin.forestTiles0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.floorLight0,sprites.dungeon.doorLockedNorth,sprites.dungeon.floorLightMoss,sprites.dungeon.floorLight5,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterEast1,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterEast1,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleOuterSouth0,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.builtin.forestTiles10,sprites.builtin.forestTiles5,sprites.builtin.forestTiles6,sprites.builtin.forestTiles7,sprites.builtin.forestTiles11,sprites.builtin.forestTiles15,sprites.builtin.forestTiles9,sprites.builtin.forestTiles13,sprites.builtin.brick,sprites.dungeon.hazardLava1,sprites.castle.tileGrass1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
